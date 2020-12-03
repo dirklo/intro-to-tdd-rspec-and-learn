2c7624839e32af850b4a2dff8479469461eda0a0
Rick <dirklo@gmail.com>
2020-12-03 20:53:41 +0100

    commit tree 887354354a42b4123636c34948a196b85497ece4

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,7 @@
+def current_age_for_birth_year(year)
+    Time.now.year - year
+end
+
+# age_of_person = current_age_for_birth_year(1984)
+# age = current_age_for_birth_year(0)
+    
--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,9 +1,21 @@
 require_relative '../current_age_for_birth_year.rb'
 
+
+describe "current_age_for_birth_year method" do
+  it "should return the current year for a person born in year 0" do
+    age = current_age_for_birth_year(0)
+ 
+    expect(age).to eq(2020)
+  end
+end
+ 
 describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
-    age_of_person = current_age_for_birth_year(1984)
-
-    expect(age_of_person).to eq(19)
+    current_year = Time.now.year
+    birth_year = 1986
+    answer = current_year - birth_year
+ 
+    age_of_person = current_age_for_birth_year(birth_year)
+    expect(age_of_person).to eq(answer)
   end
 end
